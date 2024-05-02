08eda0eaace7426c3dbc36cded9e45c90ced9000
UmitAkbas <umit.akbas@hotmail.com.tr>
2024-05-02 22:35:04 +0300

    student component changed

--- vitetest/src/App.jsx
+++ vitetest/src/App.jsx
@@ -3,12 +3,17 @@ import './App.css'
 import Student from './component/Student'
 function App() {
  
+  const student={
+    name:"John",
+    sur:"Doe",
+    num:123
+  }
 
   return (
     <>
     <h4>Student List</h4>
     <hr />
-    
+    <Student data={student}/>
     <Student name="John" sur="Doe" num={123} isClass={true}/>
     <Student name="Jane" sur="Doe" num={124}  isClass={false}/>
     <Student name="Homer" sur="Doe" num={125} isClass={true}/>
--- vitetest/src/component/Student.jsx
+++ vitetest/src/component/Student.jsx
@@ -1,9 +1,15 @@
-function Student ({name,sur,num,isClass}){
+function Student (props){
+  
+    console.log(props)
+return(
+    <div>   
+        <hr />
+        <h5>Name:{props.name}</h5>
+        <h5>Surname:{props.sur}</h5>
+        <h5>Number:{props.num}</h5>
+    </div>
+)
 
-    const isHere=isClass ? `${name} ${sur} ${num}` : `${name } Not Here`;
-    return(
-        <h3>Name&Surname: {isHere}</h3>
-    )
 }
 
 export default Student
